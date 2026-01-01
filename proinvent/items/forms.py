309dec389f94636c36843369444e3d05f7c59e6b
HaRi-Haran-D <hvj58128@gmail.com>
2026-01-01 21:39:15 +0530

    project update

--- proinvent/items/forms.py
+++ proinvent/items/forms.py
@@ -13,3 +13,16 @@ class NewItemForm(forms.ModelForm):
             'price': forms.NumberInput(attrs={'class': 'form-control'}),
             'image': forms.FileInput(attrs={'class': 'form-control'}),
         }
+
+class EditItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ['name', 'description', 'price','image','is_sold']
+
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 1}),
+            'price': forms.NumberInput(attrs={'class': 'form-control'}),
+            'image': forms.FileInput(attrs={'class': 'form-control'}),
+            'is_sold': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+        }
--- proinvent/items/views.py
+++ proinvent/items/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Item
-from .forms import NewItemForm
+from .forms import NewItemForm, EditItemForm
 
 # Create your views here.
 def detail(request, pk):
@@ -11,16 +11,30 @@ def detail(request, pk):
 
 @login_required
 def new(request):
-        if request.method == 'POST':
-              form = NewItemForm(request.POST, request.FILES)
-              if form.is_valid():
-                    item = form.save(commit=False)
-                    item.created_by = request.user
-                    item.save()
-                    return redirect('items:detail', pk=item.id)
-        else:
-            form = NewItemForm()
-        return render(request, 'items/form.html', {'form': form, 'title':'New item'})
+    if request.method == 'POST':
+        form = NewItemForm(request.POST, request.FILES)
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.created_by = request.user
+            item.save()
+            return redirect('items:detail', pk=item.id)
+    else:
+        form = NewItemForm()
+    return render(request, 'items/form.html', {'form': form, 'title':'New item'})
+
+
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+        if form.is_valid():
+            item.save()
+            return redirect('items:detail', pk=item.id)
+    else:
+        form = EditItemForm(instance=item)
+    return render(request, 'items/form.html', {'form': form, 'title':'Edit item'})
+
 
 @login_required
 def delete(request, pk):
