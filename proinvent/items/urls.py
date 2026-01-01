d8e305ef20b505767a65a9f0bfbb5e38a665aad7
HaRi-Haran-D <hvj58128@gmail.com>
2026-01-01 23:23:00 +0530

    project update

--- proinvent/items/urls.py
+++ proinvent/items/urls.py
@@ -3,6 +3,7 @@ from . import views
 app_name = 'items'
 
 urlpatterns = [
+    path('', views.items, name='list'),
     path('<int:pk>/', views.detail, name='detail'),
     path('new/', views.new, name='new'),
     path('<int:pk>/delete', views.delete, name='delete'),
--- proinvent/items/views.py
+++ proinvent/items/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.db.models import Q
 from django.contrib.auth.decorators import login_required
-from .models import Item
+from .models import Item, Category
 from .forms import NewItemForm, EditItemForm
 
 # Create your views here.
@@ -8,6 +9,19 @@ def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
     related_items = Item.objects.filter(category=item.category,is_sold=False).exclude(pk=item.pk)[0:3]
     return render(request, 'items/detail.html', {'item': item, 'related_items': related_items})
+
+def items(request):
+    query = request.GET.get('query', '').strip()
+    categories = Category.objects.all()
+    category_id = request.GET.get('category', 0)
+    items = Item.objects.filter(is_sold=False)
+
+    if category_id:
+        items = items.filter(category_id=category_id)
+
+    if query:
+        items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
+    return render(request, 'items/items.html', {'items':items, 'query':query, 'categories':categories, 'category_id':int(category_id)})
 
 @login_required
 def new(request):
